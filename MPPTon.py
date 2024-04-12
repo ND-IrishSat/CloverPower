8a4e86bfed1ba6dfae9c84d021b935115164926d
vcoppo23 <valentinecoppo@gmail.com>
2024-04-11 20:25:43 -0400

    Write i2c for enabling the MPPT

---
+++ MPPTon.py
@@ -0,0 +1,35 @@
+import smbus
+
+# Create an instance of the I2C bus
+bus = smbus.SMBus(1)  
+
+# Device I2C address (replace with your device's address)
+device_address = 0x6B  
+
+# Register address to write to (replace with your register's address)
+## watchdog registe
+register_address = 0x10
+## ICO register
+register_address2 = 0x0F
+## Enable MPPT address
+register_address3 = 0x15
+
+# Set the bit at bit_position
+## Watchdog OFF
+new_value = 0b10000000
+## ICO enable
+new_value2 = 0b10110010
+## EN MPPT
+new_value3 = 0b10101011
+
+
+
+
+
+# Read the current value of the register
+bus.write_byte_data(device_address, register_address, new_value)
+bus.write_byte_data(device_address, register_address2, new_value2)
+bus.write_byte_data(device_address, register_address3, new_value3)
+
+
+         
