a451a98c5dd0ef96d98f28ed0fa86124f018c509
BeegScoop <bfclashman15@gmail.com>
2024-11-12 19:33:09 -0500

    sample functions added

---
+++ State_Machine/sample_state_functions.c
@@ -0,0 +1,56 @@
+#include "i2c_utils.h"
+#include <stdio.h>
+#include <string.h>
+
+void idle(){
+    // idle
+}
+void detumbling(){
+    // Need:
+    // Sensors
+    // Magnotorquers
+    // Flight Computer
+}
+void idleWithSun(){
+    // Need:
+    // IMU
+    // Magnetorquers
+    // Flight Computer
+    // Solar Panel DONE ONCE
+    // Antenna Deployment DONE ONCE
+
+}
+void idleWithoutSun(){
+    // Need:
+    // IMU
+    // Magnetorquers
+    // Flight Computer
+    // Heat CONDITIONALLY
+}
+void payload(){
+    // Need:
+    // IMU
+    // Magnetorquers
+    // Payload
+    // Flight Computer
+    // Motors CONDITIONALLY
+}
+void uplinkDownlink(){
+    // Need:
+    // IMU
+    // Magnetorquers
+    // Flight Computer
+    // Motors
+    // Sidekiq Z2
+}
+void lowChargeSunPointing(){
+    // Need:
+    // IMU
+    // Magnetorquers
+    // Flight Computer
+    // Motors
+}
+void sleep(){
+    // Need:
+    //honk shoo
+}
